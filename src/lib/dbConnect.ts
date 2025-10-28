34dd743c293fa5b412721bdc49dd292012aec97a
Harshit Negi <harshitnegipro@gmail.com>
2025-10-28 19:51:29 +0530

    connect to db

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,30 @@
+import { Connection } from './../../node_modules/mongodb/src/cmap/connection';
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected? : number
+}
+
+const connection : ConnectionObject = {};
+
+async function dbConnect() : Promise<void>{
+    if(connection.isConnected){
+        console.log("Already Connected to database");
+        return;
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || '', {});
+
+        connection.isConnected = db.connections[0].readyState;
+
+        console.log("DB connected successfuly")
+
+    } catch (error) {
+        console.log("Database connection failed.", error);
+        process.exit(1);
+    }
+}
+
+
+export default dbConnect;
